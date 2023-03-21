fde88409ea211f282d9a7d196f853ffee288703a
Alexey Driganets <driganetc@mail.ru>
2023-03-21 22:01:46 +0300

    Задание 28

---
+++ Task_28/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 51); // [1, 50]
+
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
