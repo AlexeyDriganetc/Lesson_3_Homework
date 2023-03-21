9b64aa11a77e1333fa7a367870b1f4fa143935bf
Alexey Driganets <driganetc@mail.ru>
2023-03-21 22:15:29 +0300

    Задание 25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,6 +3,11 @@ Console.Write("Введите 1-ое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
 int m = Convert.ToInt32(Console.ReadLine());
+while (m < 0) 
+{
+    Console.Write("Вы ошиблись!\nВторое число должно быть положительное: ");
+    m = int.Parse(Console.ReadLine()!);
+}
 int result = 1;
 
 for (int i = 1; i <= m; i++)
